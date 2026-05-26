02ae88d898b9ef26fa0629644505fa420c44e5c7
Carnicer-Matthis <matthis.carnicer@edu.univ-fcomte.fr>
2026-05-26 13:35:42 +0200

    commit

---
+++ backend/pocketbase_odysigne/pb_migrations/1779620136_updated_abonnements.js
@@ -0,0 +1,45 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3416414251")
+
+  // remove field
+  collection.fields.removeById("date2220277813")
+
+  // add field
+  collection.fields.addAt(3, new Field({
+    "help": "",
+    "hidden": false,
+    "id": "number2220277813",
+    "max": null,
+    "min": null,
+    "name": "duree",
+    "onlyInt": false,
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "number"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3416414251")
+
+  // add field
+  collection.fields.addAt(3, new Field({
+    "help": "",
+    "hidden": false,
+    "id": "date2220277813",
+    "max": "",
+    "min": "",
+    "name": "duree",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "date"
+  }))
+
+  // remove field
+  collection.fields.removeById("number2220277813")
+
+  return app.save(collection)
+})
